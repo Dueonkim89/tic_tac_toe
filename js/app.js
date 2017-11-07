db7e46f044764fc0aee437fe845c4f0e3221bb52
Dueon Kim <dueonkim89@yahoo.com>
2017-11-07 12:14:28 -0800

    rewrote global vars as keys of ticTacToe obj, user prompt added

--- js/app.js
+++ js/app.js
@@ -1,31 +1,37 @@
-//Global DOM References
-const board = document.getElementById('board');
-const header = document.querySelector('header');
-const headline = document.querySelector('h1');
-const body = document.querySelector('body');
-
-
-
-const ticTacToe = function() {
+const ticTacToe = {
+	//Global DOM References
+	DOMReferences: 	{
+						board: document.getElementById('board'),
+						header: document.querySelector('header'),
+						headline: document.querySelector('h1'),
+						body: document.querySelector('body'),
+						player1: document.getElementById('player1'),
+						player2: document.getElementById('player2')
+					},		
+			
+	userPromptQuestion: 'What is your name?',
+	userFriendPromptQuestion: "What is your friend's name?",
 	
+	//Constructor function for the players, to be worked on....
+	createPlayer: function(name) {
+		this.name = name;
+		this.turn = false;
+	},
+		
 	
-	
-	
-	
-	this.initialize = function() {
-		console.log('Hello World');
-		board.style.display = 'none';
+	initialize: function() {
+		this.DOMReferences.board.style.display = 'none';
 		this.startPage();
-	};
+	},
 
-
-	this.startPage= function() {
+	startPage: function() {
+		//create DOM elements for start page and inject into DOM
 		const mainDiv = document.createElement('div');
 		const friendButton = document.createElement('a');
 		const computerButton = document.createElement('a');
 		const h1 = document.createElement('h1');
 		const header = document.createElement('header');
-		body.insertBefore(mainDiv, board);
+		this.DOMReferences.body.insertBefore(mainDiv, board);
 		mainDiv.className = "screen screen-start";
 		friendButton.className = "button";
 		friendButton.id = "friendButton";
@@ -38,7 +44,49 @@ const ticTacToe = function() {
 		header.appendChild(h1);
 		header.appendChild(friendButton);
 		header.appendChild(computerButton);
-	};
+		
+		//delegated event listener for the two buttons
+		this.DOMReferences.body.addEventListener('click', function (event) {
+			if (event.target.tagName === 'A' && event.target.id === 'friendButton') {
+				ticTacToe.playWithFriend();
+				
+			} else if (event.target.tagName === 'A' && event.target.id === 'computerButton') {
+				ticTacToe.playWithComputer();
+			}
+		});
+	},
+	
+	playWithFriend: function() {
+		const startDiv = document.querySelector('.screen-start');
+		const userName = this.checkIfNameValid(this.userPromptQuestion);
+		const friendName = this.checkIfNameValid(this.userFriendPromptQuestion);
+		const firstPlayer = new this.createPlayer(userName);
+		const secondPlayer = new this.createPlayer(friendName);
+		
+		console.log(firstPlayer.name);
+		console.log(secondPlayer.name);
+		
+		
+		this.DOMReferences.board.style.display = '';
+		startDiv.style.display = 'none';
+		//2 constructor functions for player names
+		//prompt for names
+		
+		
+	},
+	
+	playWithComputer: function() {
+		console.log('research minimax algorithm');
+	},
+	
+	checkIfNameValid: function(question) {
+		const validName = prompt(question);
+		if (validName === '' || validName === null) {
+			return this.checkIfNameValid(question);
+		} else {
+			return validName;
+		}
+	}
 
 
 
